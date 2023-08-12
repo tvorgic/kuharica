3a4581c7a7eac30d8d8ecd0bf140a5a900690e3e
Tonko Vorgić <117771377+tvorgic@users.noreply.github.com>
2023-08-12 19:59:12 +0200

    router page / 404 / route to controller

--- kuharica.hr/index.php
+++ kuharica.hr/index.php
@@ -1,17 +1,8 @@
 <?php
 
 require 'functions.php';
+require 'router.php';
 
 
-//parsing -> converting data from one format to another
-$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
 
-$routes = [
-  '/' => 'controllers/index.php',
-  '/about' => 'controllers/about.php',
-  '/contact' => 'controllers/contact.php',
-];
 
-if (array_key_exists($uri, $routes)){
-  require $routes[$uri];
-}
