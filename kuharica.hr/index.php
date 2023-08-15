ffbaf77523b4744550e355360f85ce362c4f6f2a
Tonko Vorgić <117771377+tvorgic@users.noreply.github.com>
2023-08-16 00:01:48 +0200

    Database class

--- kuharica.hr/index.php
+++ kuharica.hr/index.php
@@ -3,19 +3,14 @@
 
 //require 'router.php';
 require 'functions.php';
-
-$dsn = "mysql:host=localhost;port=3306;dbname=kuharica;user=root;charset=utf8mb4";
-
-$pdo = new PDO($dsn);
-
-
-$statement = $pdo->prepare("select * from recept");
-$statement->execute();
-
-$recepti = $statement->fetchAll();
-
-dd($recepti);
+require 'Database.php';
 
 
 
 
+$db = new Database();
+
+$recept = $db->query("select * from recept")->fetchAll(PDO::FETCH_ASSOC);
+
+
+dd($recept);
