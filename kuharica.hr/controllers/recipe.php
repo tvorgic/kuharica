8ad47a5eb9143ceac7a91a24d171fac10695fcd1
Tonko Vorgić <117771377+tvorgic@users.noreply.github.com>
2023-08-17 00:09:29 +0200

    Response

--- kuharica.hr/controllers/recipe.php
+++ kuharica.hr/controllers/recipe.php
@@ -11,6 +11,16 @@ $heading = 'Recipe';
 
 $recipe = $db->query('select * from recept where sifra = :sifra', ['sifra' => $_GET['sifra']])->fetch();
 
+$currentUser = 2;
+
+if(! $recipe) {
+  abort();
+}
+
+if ($recipe ['sifra'] !== $currentUser) {
+  abort(Response::FORBIDDEN);
+}
+
 require 'views/recipe.view.php';
 
 
