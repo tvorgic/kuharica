a94cffd084077308f047c984d7dab88a35dff826
Tonko Vorgić <117771377+tvorgic@users.noreply.github.com>
2023-08-22 21:28:14 +0200

    SESSIONS start

--- kuharica.hr/controllers/index.php
+++ kuharica.hr/controllers/index.php
@@ -1,4 +1,5 @@
 <?php
+$_SESSION['ime'] = 'Tonko';
 
 view('index.view.php', [
   'heading' => 'Home'
--- kuharica.hr/views/partials/banner.php
+++ kuharica.hr/views/partials/banner.php
@@ -1,10 +1,9 @@
 
 <header class="bg-white shadow">
     <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-      <h1 class="text-3xl font-bold tracking-tight text-gray-900"><?=$heading?></h1>
+      <h1 class="text-3xl font-bold tracking-tight text-gray-900">Hello, <?=$_SESSION['ime'] ?? 'Guest'?></h1>
     </div>
   </header>
-
   
   <main>
     <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
