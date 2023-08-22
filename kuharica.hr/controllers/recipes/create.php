07a2db75ab90cf4877070e3c1520f3c86ee0d775
Tonko Vorgić <117771377+tvorgic@users.noreply.github.com>
2023-08-22 10:40:30 +0200

    store.php

--- kuharica.hr/controllers/recipes/create.php
+++ kuharica.hr/controllers/recipes/create.php
@@ -1,31 +1,11 @@
 <?php
 
-use Core\Database;
-use Core\Validator;
-
-$config = require base_path('config.php');
-$db = new Database($config['database']);
-
 $errors = []; 
 
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST'){
-  $errors = [];  
-
-  if (! Validator::string($_POST['opis'], 1, 10000)){
-    $errors['opis'] = 'Recipe description of no more than 10 000 characters is required';
-  }
-
-    if (empty($errors)) {
-      $db->query('INSERT INTO recept (naziv, opis) VALUES(:naziv, :opis)', [
-        'naziv' => $_POST['naziv'],
-        'opis' => $_POST['opis']
-      ]);
-    }
-}
 
 
 view('recipes/create.view.php', [
   'heading' => 'Create Recipe',
-  'errors' => $errors
+  'errors' => []
 ]);
--- kuharica.hr/controllers/recipes/store.php
+++ kuharica.hr/controllers/recipes/store.php
@@ -1,3 +1,34 @@
 <?php
 
-dd('hello');
+use Core\Validator;
+use Core\Database;
+
+$config = require base_path('config.php');
+$db = new Database($config['database']);
+
+$rerrors = [];
+
+
+if (! Validator::string($_POST['opis'], 1, 10000)){
+
+ $errors['opis'] = 'Recipe description of no more than 10 000 characters is required';
+  }
+
+
+
+  if (! empty($errors)) {
+
+   return view('recipes/create.view.php', [
+      'heading' => 'Create Recipe',
+      'errors' => $errors
+    ]);
+
+  }  
+    $db->query('INSERT INTO recept (naziv, opis) VALUES(:naziv, :opis)', [
+      'naziv' => $_POST['naziv'],
+      'opis' => $_POST['opis']
+     ]);
+
+     header('location: /recipes');
+     die();
+  
