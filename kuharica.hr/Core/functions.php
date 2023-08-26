647cd5d73c496f12cea5d67b96bd876fe4dda310
Tonko Vorgić <117771377+tvorgic@users.noreply.github.com>
2023-08-26 18:33:00 +0200

    automatic redirect back to login

--- kuharica.hr/Core/functions.php
+++ kuharica.hr/Core/functions.php
@@ -46,3 +46,7 @@ function redirect($path) {
   header("location: {$path}");
   exit();
 }
+
+function old($key, $default = '') {
+  return Core\Session::get('old')[$key] ?? $default;
+}
--- kuharica.hr/Http/controllers/session/store.php
+++ kuharica.hr/Http/controllers/session/store.php
@@ -19,5 +19,8 @@ if ($form->validate($email, $pass)) {
 
 
 Session::flash('errors', $form->errors());
+Session::flash('old', [
+    'email' => $_POST['email']
+]);
 
 return redirect('/login');
