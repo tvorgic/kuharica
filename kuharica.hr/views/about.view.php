63f058e2910efb4261967ebab145e98d5b42c920
Tonko Vorgić <117771377+tvorgic@users.noreply.github.com>
2023-08-30 21:42:23 +0200

    quick update

--- kuharica.hr/views/about.view.php
+++ kuharica.hr/views/about.view.php
@@ -2,7 +2,17 @@
 <?php require 'partials/nav.php'?>
 <?php require 'partials/banner.php'?>
 
+    <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
 
+      <br>
+      
+      <p>I am a begginer web developer working in PHP trying to make a simple web site. Here you can find some recipes for easy to make meals with all the ingredients. Have fun</p>
+
+    </div>
+   
+
+
+  
 
  
 <?php require 'partials/footer.php'?>
--- kuharica.hr/views/contact.view.php
+++ kuharica.hr/views/contact.view.php
@@ -1,5 +1,17 @@
 <?php require 'partials/head.php'?>
 <?php require 'partials/nav.php'?>
 <?php require 'partials/banner.php'?>
+
+    <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+
+      <br>
+      
+      <p>You can contact me via email address: tonkokuharica@gmail.com</p>
+
+    </div>
+   
+
+
+  
  
 <?php require 'partials/footer.php'?>
